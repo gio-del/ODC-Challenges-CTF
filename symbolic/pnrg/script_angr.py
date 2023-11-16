2967f275e805edd0776b527c11a3438d88f4e269
gio-del <giovanni.delucia@polimi.it>
2023-11-16 16:11:53 +0100

    pnrg

---
+++ symbolic/pnrg/script_angr.py
@@ -0,0 +1,35 @@
+import angr
+import claripy
+from pwn import *
+
+strlen = 30
+
+prog = angr.Project('./prodkey')
+
+find = [0x0010167a] # key ok
+avoid = [0x001016e5] # key ko
+
+input_flag = claripy.BVS('seed', strlen * 8)
+
+state = prog.factory.entry_state(stdin=input_flag)
+simgr = prog.factory.simulation_manager(state)
+
+simgr.explore(find=find, avoid=avoid)
+
+if simgr.found:
+	found = simgr.found[0]
+	flag = found.solver.eval(input_flag)
+	flag = bytes.fromhex(hex(flag)[2:]).decode('utf-8')
+
+	print('flag:', flag, 'len: ')
+
+	p = remote("bin.training.offdef.it", 2021)
+
+	p.recvuntil(b'\nPlease Enter a product key to continue: \n')
+	p.sendline(flag)
+
+	print('Flag:', str(p.recv(),'utf-8'))
+
+
+
+
